27e15b89db9f44b74683855e1d55995d215be1bf
+ <+>
2022-06-29 16:12:08 -0400

    commit tree 7536232fa25d38968830e557ca6cc0f16eba2a81

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(hobby = "roller-skate"){{
+    return `This Saturday, I want to ${hobby}!`}
+};
+function mondayWork(work = "go to the office") {
+    {return `This Monday, I will ${work}.`}
+};
+function wrapAdjective (flair= '*') {
+    return function(adjective = "special"){
+        return`You are ${flair}${adjective}${flair}!`
+    }
+}
